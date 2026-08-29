8827de1a94a2a7b822e441a2cfde789f478ee0f9
lucas dias xavier <lucasdx1993@outlook.com>
2026-08-29 02:06:43 -0300

    refactor: componentize application structure

---
+++ js/loadComponents.js
@@ -0,0 +1,12 @@
+async function loadComponent(id, file) {
+  const response = await fetch(file);
+
+  const component = await response.text();
+
+  document.getElementById(id).innerHTML = component;
+}
+
+loadComponent(
+  "header-component",
+  "./components/header.html"
+);
